7ab114747c08ab29fcedd6885c43141e197666c4
Moneshsoni <sonimonesh115@gmail.com>
2023-06-08 11:53:16 +0530

    add the dapp project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
-
+require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity:{
+    compilers:[
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.8.2"
+      }
+    ]
+  },
+  defaultNetwork: "hardhat",
+  networks:{
+    hardhat: {
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/X1H7etINPr6jELA9HL7ofrvMHGnyhBcU`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan:{
+    apiKey:{
+      polygonMumbai: 'KDUE344NY1N55XVGURHMWTA13IK1PK2SVG'
+    }
+  }
 };
+
+
+// https://mumbai.polygonscan.com/address/0x508B35fBa25Da3B706Fd5F4A715596122f7F5831#code
---
+++ medium-project/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
